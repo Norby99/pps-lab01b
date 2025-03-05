d1dbd20df788b23afd4aadd0a6056e7cc8803d76
Norbert Gabos <dont.vip@gmail.com>
2025-03-05 17:58:25 +0100

    remove useless bank accounts

--- src/main/java/e1/BronzeBankAccount.java
+++
@@ -1,7 +0,0 @@
-package e1;
-
-public class BronzeBankAccount extends CustomFeeBankAccountDecorator {
-    public BronzeBankAccount(BankAccount base) {
-        super(base, amount -> amount < 100 ? 0 : 1); // Silver ha una fee di 1
-    }
-}
--- src/main/java/e1/GoldBankAccount.java
+++
@@ -1,9 +0,0 @@
-package e1;
-
-public class GoldBankAccount extends OverdraftBankAccountDecorator {
-    private final static int OVERDRAFT = 500;
-
-    public GoldBankAccount(BankAccount base) {
-        super(base, OVERDRAFT);
-    }
-}
